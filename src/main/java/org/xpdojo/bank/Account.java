7e14be9f9266e6bb3c424ed2ab40311ba00dda32
Davide Martorana <davide.martorana@gmail.com>
2021-01-28 15:29:39 +0000

    Green test

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,9 +1,18 @@
 package org.xpdojo.bank;
 
+import java.math.BigDecimal;
+
 public class Account {
 
+    private Money balance = Money.getInstance("0");
+
     public Money deposit(final Money money) {
-        return null;
+        if(money.getAmount().compareTo(BigDecimal.ZERO) < 0){
+            throw new IllegalArgumentException("Negative amount of money cannot be deposited.");
+        }
+        final BigDecimal newAmount = balance.getAmount().add(money.getAmount());
+        balance = new Money(newAmount);
+        return balance;
     }
 
 }
--- src/main/java/org/xpdojo/bank/Money.java
+++ src/main/java/org/xpdojo/bank/Money.java
@@ -2,14 +2,15 @@ package org.xpdojo.bank;
 
 import java.math.BigDecimal;
 import java.util.Currency;
+import java.util.Objects;
 
 /**
  * Immutable class to represent Money as a concept.
  * This class should have no accessor methods.
  */
 public class Money {
-    final BigDecimal amount;
-    final Currency currency;
+    private final BigDecimal amount;
+    private final Currency currency;
 
     public static Money getInstance(String amount){
         return new Money(new BigDecimal(amount));
@@ -26,5 +27,18 @@ public class Money {
 
     public BigDecimal getAmount() {
         return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return Objects.equals(amount, money.amount) && Objects.equals(currency, money.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, currency);
     }
 }
